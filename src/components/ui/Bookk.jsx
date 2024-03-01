2c12d26ce5209d1ac3b043d49900b17fb55d2855
sethdimmer234 <sethdimmer1@gmail.com>
2024-03-01 15:17:13 -0500

    initial commit

--- src/components/ui/Bookk.jsx
+++ src/components/ui/Bookk.jsx
@@ -1,63 +1,72 @@
+
+
 import React, { useEffect, useRef, useState } from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faStar, faStarHalfAlt } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Price from "./Price";
 
 const Bookk = ({ book }) => {
-  const [img,setImg] = useState()
-
-  const mountedRef = useRef(true)
+  const [img, setImg] = useState();
+  const mountedRef = useRef(true);
 
   useEffect(() => {
-    const image = new Image()
+    const image = new Image();
     image.src = book.url;
     image.onload = () => {
       setTimeout(() => {
-        if(mountedRef.current) {
+        if (mountedRef.current) {
           setImg(image);
-
         }
-      }, 300)
+      }, 300);
     };
-    return () => {
-      //when the component unmounts
-      mountedRef.current = false
-    }
-  })
 
+    return () => {
+      // Cleanup function
+      mountedRef.current = false;
+    };
+  }, [book.url]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+
+    // Cleanup function
+    return () => {
+    };
+  }, []);
 
   return (
     <div className="book">
-    {img ? (
-      <>
-      <Link to={`/books/${book.id}`}>
-        <figure className="book__img--wrapper">
-          <img src={img.src} alt="" className="book__img"  />
-        </figure>
-      </Link>
-      <div className="book__title">
-        <Link to={`/books/${book.id}`} className="book__title--link">
-          {book.title}
-        </Link>
-      </div>
-      <Rating rating={book.rating}/>
-      <Price salePrice={book.salePrice} originalPrice={book.originalPrice}/>
-      </>
-  ) :(
-    <>
-      <div className="book__img--skeleton"></div>
-      <div className="skeleton book__title--skeleton"></div>
-      <div className="skeleton book__rating--skeleton"></div>
-      <div className="skeleton book__price--skeleton"></div>
-    </>
-    )}
+      {img ? (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__img--wrapper">
+              <img src={img.src} alt="" className="book__img" />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Rating rating={book.rating} />
+          <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+        </>
+      ) : (
+        <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
+        </>
+      )}
     </div>
-  
   );
 };
 
 export default Bookk;
+
+
+
+
 
 
